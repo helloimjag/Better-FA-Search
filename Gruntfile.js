3f28b40ca16006c10d5f11d1fca28198b9ccc004
JAG <jag@helloimjag.com>
2017-05-06 15:42:41 -0700

    Automated tasks to deploy on github pages

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        cssmin: {
+            maincss: {
+                files: [{
+                    src: ["main.css"],
+                    dest: "docs/main.min.css"
+                }]
+            }
+        },
+        concat: {
+            js: {
+                src: [
+                    "app/app.js",
+                    "app/icons/iconController.js",
+                    "app/icons/iconService.js"
+                ],
+                dest: "docs/app/app.js"
+            }
+        },
+        uglify: {
+            appjs: {
+                files: {
+                    "docs/app/app.min.js": ["docs/app/app.js"]
+                }
+            }
+        },
+        copy: {
+            moveindex: {
+                src: "index.html",
+                dest: "docs/index.html"
+            },
+            fajson: {
+                src: "fa/fa.json",
+                dest: "docs/fa/fa.json"
+            }
+        },
+        injector: {
+            options: {
+                ignorePath: "docs",
+                addRootSlash:false
+            },
+            dev: {
+                files: {
+                    "index.html": [
+                        "main.css",
+                        "node_modules/angular/angular.min.js",
+                        "node_modules/clipboard/dist/clipboard.min.js",
+                        "app/*.js",
+                        "app/*/*.js"
+                    ]
+                }
+            },
+            build: {
+                files: {
+                    "docs/index.html": [
+                        "docs/*.min.css",
+                        "docs/js/*.min.js",
+                        "docs/app/*.min.js"
+                    ]
+                }
+            }
+        },
+        removeHtmlComments: {
+            cleanhtml: {
+              src:"docs/index.html",
+              dest:"docs/index.html"
+            }
+        },
+        htmlmin:{
+          build:{
+            options: {
+              removeComments: true,
+              collapseWhitespace: true
+            },
+            files:{
+              "docs/index.html":"docs/index.html"
+            }
+          }
+        }
+
+    });
+
+    // Load required modules
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-injector");
+    grunt.loadNpmTasks("grunt-stripcomments");
+    grunt.loadNpmTasks("grunt-remove-html-comments");
+    grunt.loadNpmTasks("grunt-contrib-htmlmin");
+
+    // Task definitions
+    grunt.registerTask("dev", ["injector:dev"]);
+
+    grunt.registerTask("build", [
+        "concat:js",
+        "cssmin:maincss",
+        "uglify:appjs",
+        "copy:moveindex",
+        "copy:fajson",
+        "injector:build",
+        "removeHtmlComments:cleanhtml",
+        "htmlmin:build"
+    ]);
+
+    // Says Hello
+    grunt.registerTask("default", "Saying Hello", function(arg) {
+        grunt.log.writeln("----------------------------------------");
+        grunt.log.writeln("----------- HELLO DEVELOPER! -----------");
+        grunt.log.writeln("----------------------------------------");
+
+    });
+};
