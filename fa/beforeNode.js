c1c3435d20bdd10d7d684cf639486e4836c9a48c
JAG <jag@helloimjag.com>
2017-11-26 01:32:59 -0800

    Removing Unused files

--- fa/beforeNode.js
+++ fa/beforeNode.js
@@ -4,8 +4,7 @@ function start(){
 function readCss(){
   function matchText(data){
     var re = "\.fa-.*:before.*\n.*content.*\n\}";
-    // var re = "(\.fa-)(.*)(:before)(.*)"; Working from .fa to before {
-    var re = new RegExp(re, 'gmi');
+    re = new RegExp(re, 'gmi');
     var matches = data.match(re);
     displayCss(matches);
   }
@@ -13,25 +12,24 @@ function readCss(){
     var faa = {data:[]};
     if(data){
       data.forEach(printFa);
-      function printFa(fa){
-        var fa = fa.replace(".fa-","")
-                   .replace(":before {","")
-                   .replace(";","")
-                   .replace("}","")
-                   .replace('"','')
-                   .replace('"','')
-                   .replace(/ /g, "")
-                   .replace("\n", "")
-                   .trim()
-                   .split("content:");
-        var obj = {}
-            obj.name = fa[0];
-            obj.code = fa[1];
-        faa.data.push(obj);
-        $("body").append(fa+"<br>");
-      }
       console.log(faa);
-      // print json ready
+    }
+    function printFa(fa){
+      fa = fa.replace(".fa-","")
+      .replace(":before {","")
+      .replace(";","")
+      .replace("}","")
+      .replace('"','')
+      .replace('"','')
+      .replace(/ /g, "")
+      .replace("\n", "")
+      .trim()
+      .split("content:");
+      var obj = {};
+      obj.name = fa[0];
+      obj.code = fa[1];
+      faa.data.push(obj);
+      $("body").append(fa+"<br>");
     }
   }
   $.get('fa/css/font-awesome.css', matchText, 'text');
