33c180aa44f4d6d15afad6c336107a91dcf9ac68
JAG <jag@helloimjag.com>
2017-11-26 01:42:52 -0800

    Modal seperated

---
+++ app/icons/modal.service.js
@@ -0,0 +1,35 @@
+(function(){
+  "use strict";
+
+  angular
+  .module("fa")
+  .factory("modalService", modalService);
+
+  modalService.$inject = ["$window"];
+
+  function modalService($window){
+    var service = {};
+    var store = $window.localStorage;
+    service.display = display;
+    service.get = check;
+
+    return service;
+
+    function display(){
+      var i = check();
+      if(i){
+        store.setItem('i', 0);
+        return !!i;
+      }else{
+        store.setItem('i', 1);
+        return !!i;
+      }
+    }
+
+    function check(){
+      var n = store.getItem('i');
+      if(n === null){return 1;}
+      return !!Number(n);
+    }
+  }
+})();
