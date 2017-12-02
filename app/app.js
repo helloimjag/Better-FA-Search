e5c9a748b47df66d80b786ca6a5e1abe70018b07
JAG <jag@helloimjag.com>
2017-12-01 17:54:54 -0800

    Rebuilt with browserify

--- app/app.js
+++ app/app.js
@@ -1,7 +1,4 @@
-(function() {
-  "use strict";
-
-  angular
-    .module("fa", []);
-
-})();
+'use strict';
+var angular = require('angular');
+var app = angular.module('fa', []);
+require('./icons');
